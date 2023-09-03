9c7ad859c5f6b6bace146913a55bee27c861e061
fadyat <fadyat@icloud.com>
2023-09-03 20:28:08 +0300

    feat: alerting errors to separate tg channel

--- api/sender/tg.go
+++ api/sender/tg.go
@@ -2,21 +2,20 @@ package sender
 
 import (
 	"context"
-	"github.com/fadyat/i4u/internal/config"
 	"github.com/fadyat/i4u/internal/entity"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Tg struct {
-	c   *tgbotapi.BotAPI
-	cfg *config.Telegram
+	c      *tgbotapi.BotAPI
+	chatID int64
 }
 
-func NewTg(c *tgbotapi.BotAPI, cfg *config.Telegram) *Tg {
-	return &Tg{c: c, cfg: cfg}
+func NewTg(c *tgbotapi.BotAPI, chatID int64) *Tg {
+	return &Tg{c: c, chatID: chatID}
 }
 
-func (t *Tg) Send(_ context.Context, msg *entity.SummaryMsg) error {
-	_, err := t.c.Send(tgbotapi.NewMessage(t.cfg.ChatID, msg.Summary))
+func (t *Tg) Send(_ context.Context, msg entity.SummaryMessage) error {
+	_, err := t.c.Send(tgbotapi.NewMessage(t.chatID, msg.Summary()))
 	return err
 }
